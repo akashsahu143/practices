d0dc1e731a7d4ac4b5442958503176b86a13f4c9
Akash kumar saw <akashsahu105677@gmail.com>
2025-05-08 10:08:59 +0000

    container config done

---
+++ 01_basic/variable.js
@@ -0,0 +1,18 @@
+const accountId = 123456 
+let accountEmail ="aks123@gmail.com"
+
+var accountPassword ="123456789"
+
+accountCity = "Jharkhand"
+
+accountState = " Ranchi"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+// let update
+
+// accountId = 789456
+// accountEmail = "ak1@g.com"
+// accountPassword = " vfjlahgvjv "
