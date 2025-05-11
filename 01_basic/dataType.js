56e4e2f49052b89c808d493c15e9e49b856cac59
Akash kumar saw <akashsahu105677@gmail.com>
2025-05-11 17:25:04 +0000

    dataType

---
+++ 01_basic/dataType.js
@@ -0,0 +1,21 @@
+"u7se strict"; // treat all js code as a newer version
+
+// alert(3+3)  // we are using Node.JS , not browser 
+
+let name = "akash"  // string deta type
+
+let age = 24  // number data type
+
+let isLoggedIn= true   // or fasle   // boolean deta type
+
+// number => 2 to power 53
+//bigint
+//dtring=> ""
+//boolean => true/false
+//null => standlone value
+//undefine => value not assigned 
+//symbol => uniqness values use moslty in react
+
+//object 
+
+console.log(typeof undefined);
