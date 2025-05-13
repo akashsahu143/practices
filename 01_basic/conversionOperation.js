1db3bb4d0cd306cb709adda5b27ad6c3a302d4ec
Akash kumar saw <akashsahu105677@gmail.com>
2025-05-13 23:46:46 +0000

    operation

--- 01_basic/conversionOperation.js
+++ 01_basic/conversionOperation.js
@@ -23,5 +23,14 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//                                 operation 
+
+
+
+let value = 3
+let negValue  = -value
+console.log(negValue);
