c44da0e6053c31f123f906e693b24eb7c45d7264
Akash kumar saw <akashsahu105677@gmail.com>
2025-05-14 05:06:29 +0000

    done

---
+++ 01_basic/dataTypes-summary.js
@@ -0,0 +1,33 @@
+//primitive
+
+// 7 types: String, number, boolean, null(empty),undefined,symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id===anotherid);
+
+
+const bigNumber = 1235468755254n
+
+// Refrance types(Non primitive)
+
+//Array, Objects, Function 
+
+const heros =["saktimaan","krish","hanuman"]
+let myObj ={
+    name:"akash",
+    age:21,
+}
+
+const myFunction= function(){
+    console.log("Hello world");
+}
