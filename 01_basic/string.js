f15cc0c4804d5af77078fdcbfe037fd8ae5bc62f
Akash kumar saw <akashsahu105677@gmail.com>
2025-05-15 10:28:21 +0000

    string

---
+++ 01_basic/string.js
@@ -0,0 +1,20 @@
+const name = "akash"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");  // this is odddated so am not recamended
+
+console.log(`hello myh name is ${name} and my repocount is ${repoCount}`);  //string implitation and morden way 
+
+
+const gameName = new String('AKash')
+console.log(gameName[0])
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
+
+
