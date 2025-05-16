f43044f454a00bfe80916ccd44a01fd5a236dae3
Akash kumar saw <akashsahu105677@gmail.com>
2025-05-16 04:29:29 +0000

    update

--- 01_basic/string.js
+++ 01_basic/string.js
@@ -6,7 +6,7 @@ const repoCount = 50
 console.log(`hello myh name is ${name} and my repocount is ${repoCount}`);  //string implitation and morden way 
 
 
-const gameName = new String('AKash')
+const gameName = new String('Akash-kumar-saw')
 console.log(gameName[0])
 console.log(gameName.__proto__);
 
@@ -17,4 +17,26 @@ console.log(gameName.toLowerCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('a'));
 
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+const anotherString = gameName.slice(-5,4)
+console.log(anotherString);
+
+const newStringOne = "     Akash     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://akash.com/akash%20blog"
+
+console.log(url.replace('%20', '-'))
+
+// you can ask question let example 
+
+console.log(url.includes('akash')) //true
+console.log(url.includes('saw'))  //false
+
+// convert string into array 
+
+console.log(gameName.split('-'));
 
