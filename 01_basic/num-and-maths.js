9c7239ef4d98cceaa805c75090d91859f64932d5
Akash kumar saw <akashsahu105677@gmail.com>
2025-05-16 06:02:52 +0000

    final update of number and math

--- 01_basic/num-and-maths.js
+++ 01_basic/num-and-maths.js
@@ -16,15 +16,20 @@ const hundreds = 1000000
 
 //                  Maths
 
-console.log(Math)
-console.log(Math.abs(-4))
-console.log(Math.abs(4))
-console.log(Math.round(4.54))
-console.log(Math.ceil(4.54))
-console.log(Math.floor(4.54))
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.abs(4))
+// console.log(Math.round(4.54))
+// console.log(Math.ceil(4.54))
+// console.log(Math.floor(4.54))
 
-console.log(Math.min(4,5,2,1,0,5))
-console.log(Math.max(4,5,2,1,0,5))
+// console.log(Math.min(4,5,2,1,0,5))
+// console.log(Math.max(4,5,2,1,0,5))
 
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10)+1)
 
-    
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random()* (max-min +1))+ min)
