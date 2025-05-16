17faaf71db36c02d441c436b712e185acb7f8d97
Akash kumar saw <akashsahu105677@gmail.com>
2025-05-16 05:40:05 +0000

    change

---
+++ 01_basic/num-and-maths.js
@@ -0,0 +1,30 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number (100)
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(1))
+
+const otherNumber = 24.5856
+
+// console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//                  Maths
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.abs(4))
+console.log(Math.round(4.54))
+console.log(Math.ceil(4.54))
+console.log(Math.floor(4.54))
+
+console.log(Math.min(4,5,2,1,0,5))
+console.log(Math.max(4,5,2,1,0,5))
+
+
+    
