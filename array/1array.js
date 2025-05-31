afd7f3ae5f7ecba925fddd6ec9a8ce6f5397f9a6
Akash kumar saw <akashsahu105677@gmail.com>
2025-05-31 05:04:05 +0000

    array

--- array/1array.js
+++ array/1array.js
@@ -1,3 +1,42 @@
 //Array
 
 const myArr = [0,1,2,3,4,5]
+// console.log(myArr[1]); // output 1 
+// console.log(myArr[0]); // output 0
+
+
+
+const myHero =["saktiman","krish"]
+
+const myArr1 = new Array (1,2,3,4,5)
+// console.log(myArr[1]);
+
+                                  //Array method
+
+
+// myArr.push(6)
+// myArr.pop() //last value is remove
+// console.log(myArr)
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.indexOf(3));
+
+
+// console.log(myArr);
+
+
+// const newArr = myArr.join()
+// console.log(newArr)
+// console.log(typeof newArr)
+
+
+              //slice , splice
+
+console.log("A",myArr)
+
+const myn1 = myArr.slice(1,3)
+console.console.log();
+
+
