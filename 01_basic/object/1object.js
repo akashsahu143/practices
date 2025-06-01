9b7955953d5095a0e68158f58f5ad5c40f6b9095
Akash kumar saw <akashsahu105677@gmail.com>
2025-06-01 05:21:27 +0000

    upadate

---
+++ 01_basic/object/1object.js
@@ -0,0 +1,16 @@
+//Singleton  by construction
+//object.create
+//object literals
+
+const mySym = symbol("key1")
+
+const jsUser = {
+    name: "Akash",
+    age: 21,
+    location: "jharkhand",
+    email: "akash123@gmail.com",
+    isLoggedIn: "false"
+}
+
+console.log(jsUser.email);
+console.log(jsUser["email"]);
